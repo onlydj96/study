009500096b11e08663e2e52af722621580adbd82
onlydj96 <94875061+onlydj96@users.noreply.github.com>
2021-11-23 20:47:42 +0900

    Add files via upload

---
+++ practice 1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+matrix = np.array(range(1, 16))
+matrix.shape = 5, 3
+print(matrix)
+
+print(type(matrix))
+
+print(matrix.size)
---
+++ test0001.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+a = 0.1 
+b = 0.2
+print(int(a + b))
+
+import tensorflow as tf
+from tensorflow.python.ops.gen_array_ops import identity
+print(tf.__version__)
+
