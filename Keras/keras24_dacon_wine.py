70f49e4351b06ea9a6232fff9f92894376fcee12
onlydj96 <94875061+onlydj96@users.noreply.github.com>
2021-12-06 17:57:09 +0900

    Add files via upload

--- Keras/keras24_dacon_wine.py
+++ Keras/keras24_dacon_wine.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
-from pandas.core.reshape.reshape import get_dummies
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler
 from tensorflow.keras.callbacks import EarlyStopping
 
 
@@ -27,9 +26,16 @@ x['type'] = le.transform(x['type'])
 le.fit(test_file['type'])
 test_file['type'] = le.transform(test_file['type'])
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1004)
+'''
+label encoding을 통해서 str로 되있는 정보들을 숫자값으로 변환시켜준다.(문자형은 계산을 못함)
+'''
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
+
+# scaler = RobustScaler()
 scaler = MinMaxScaler()
+# scaler = StandardScaler()
+# scaler = MaxAbsScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
@@ -39,26 +45,31 @@ test_file = scaler.transform(test_file)
 # print(x.shape, y.shape) # (3231, 12) (3231,)
 
 model = Sequential()
-model.add(Dense(60, input_dim=x.shape[1]))
-model.add(Dense(42, activation='relu'))
-model.add(Dense(35))
-model.add(Dense(25))
-model.add(Dense(12))
+model.add(Dense(100, input_dim=x.shape[1]))
+model.add(Dense(60, activation='relu'))
+model.add(Dense(40, activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(10, activation='relu'))
 model.add(Dense(5, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-es = EarlyStopping(monitor='val_loss', patience=50, mode='min', verbose=1, restore_best_weights=True)
-model.fit(x_train, y_train, epochs=1000, validation_split=0.2, batch_size=16, callbacks=[es])
+es = EarlyStopping(monitor='val_loss', patience=300, mode='min', verbose=1, restore_best_weights=True)
+model.fit(x_train, y_train, epochs=10000, validation_split=0.2, verbose=3, batch_size=10, callbacks=[es])
 
-model.save("./_save/dacon_wine1.h5")
 
 loss = model.evaluate(x_test, y_test)
 print("loss, accuracy : ", loss)
 
+acc = str(round(loss[1], 4))
+model.save("./_save/dacon_wine_{}.h5".format(acc))
+
 ############################## 제출용 #####################################
 
 result = model.predict(test_file)
-result_recover = np.argmax(result, axis=1).reshape(-1, 1) +4
+'''
+argmax 함수를 사용하여 다중분류로 예측된 값들중 가장 높은 값을 선택하여준다.
+'''
+result_recover = np.argmax(result, axis=1).reshape(-1, 1) +4  # +4를 해줌으로 quality의 기준값들에 
 submit_file['quality'] = result_recover
 # print(result_recover[:20])
 # print(np.unique(result_recover))
