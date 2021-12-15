4435e5b101cfcace88a2d2bbbbe9ff5cf2ea565e
onlydj96 <94875061+onlydj96@users.noreply.github.com>
2021-12-15 20:27:22 +0900

    Add files via upload

---
+++ Numpy/1st dim.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+x = np.array(range(1, 11))
+'''
+# 자료형 출력
+print(type(x))
+
+# 차원을 출력
+print(x.ndim)
+
+# 모양을 출력
+print(x.shape)
+
+# 크기를 출력
+print(x.size)
+
+# dtype(date type)을 출력
+print(x.dtype)
+
+# 인덱스 5의 요소 출력
+print(x[5])
+
+# 인덱스 3-5의 요소 출력
+print(x[3:6])
+'''
