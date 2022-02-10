320264aa0421fffbce15d2493f569d0191eb8071
Bodong <94875061+onlydj96@users.noreply.github.com>
2022-02-10 17:24:47 +0900

    Add files via upload

---
+++ tf114/tf14_4_cancer.py
@@ -0,0 +1,49 @@
+from sklearn.datasets import load_breast_cancer
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+tf.compat.v1.set_random_seed(66)
+
+datasets = load_breast_cancer()
+x_data = datasets.data
+y_data = datasets.target
+y_data = y_data.reshape(y_data.shape[0], -1)
+print(x_data.shape)
+
+
+x = tf.compat.v1.placeholder(tf.float32, shape=[None, x_data.shape[1]])
+y = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
+
+w = tf.compat.v1.Variable(tf.random.normal([x_data.shape[1], 1]), name='weight')
+b = tf.compat.v1.Variable(tf.random.normal([1]), name='bias')  
+
+#2. 모델
+hypothesis = tf.sigmoid(tf.matmul(x, w) + b)
+
+#3-1. 컴파일
+# loss = tf.reduce_mean(tf.square(hypothesis - y))    # mse
+loss = -tf.reduce_mean(y*tf.math.log(hypothesis) + (1-y)*tf.math.log(1-hypothesis))  # binary_crossentropy
+optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(loss)
+
+#3-2. 훈련
+sess = tf.compat.v1.Session()
+sess.run(tf.compat.v1.global_variables_initializer())
+
+for epoch in range(2001):
+    loss_val, hy_val, _= sess.run([loss, hypothesis, train], feed_dict={x:x_data, y:y_data})
+    
+    if epoch % 200 == 0:
+        print(epoch, '\t', loss_val)
+
+
+#4. 평가, 예측
+y_predict = tf.cast(hypothesis >= 0.5, dtype=tf.float32)  
+
+# accuracy score
+accuracy = tf.reduce_mean(tf.cast(tf.equal(y, y_predict), dtype=tf.float32))
+pred, acc = sess.run([y_predict, accuracy], feed_dict={x:x_data, y:y_data})
+
+print("Accuracy - \n" , acc)
+
+sess.close()
---
+++ tf114/tf15_1_softmax.py
@@ -0,0 +1,52 @@
+import numpy as np
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+tf.compat.v1.set_random_seed(66)
+
+x_data = [[1, 2, 1, 1],
+          [2, 1, 3, 2],
+          [3, 1, 3, 4],
+          [4, 1, 5, 5],
+          [1, 7, 5, 5],
+          [1, 2, 5, 6],
+          [1, 6, 6, 6],
+          [1, 7, 6, 7]]
+y_data = [[0, 0, 1],
+          [0, 0, 1],
+          [0, 0, 1],
+          [0, 1, 0],
+          [0, 1, 0],
+          [0, 1, 0],
+          [1, 0, 0],
+          [1, 0, 0]]
+
+x_predict = [[1, 11, 7, 9]]
+
+#2. 모델 구성
+x = tf.compat.v1.placeholder(tf.float32, shape=[None, 4])
+y = tf.compat.v1.placeholder(tf.float32, shape=[None, 3])
+
+w = tf.compat.v1.Variable(tf.random.normal([4, 3]), name='weight')
+b = tf.compat.v1.Variable(tf.random.normal([1, 3]), name='bias')  
+
+#2. 모델
+hypothesis = tf.nn.softmax(tf.matmul(x, w) + b)
+
+#3-1. 컴파일
+loss = tf.reduce_mean(-tf.reduce_sum(y * tf.math.log(hypothesis), axis=1))    # categorical_crossentropy
+optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
+
+#3-2. 훈련
+with tf.compat.v1.Session() as sess:
+    sess.run(tf.compat.v1.global_variables_initializer())
+    
+    for epoch in range(20001):
+        _, loss_val = sess.run([optimizer, loss], feed_dict={x:x_data, y:y_data})
+        
+        if epoch % 200 == 0:
+            print(epoch, '\t', loss_val)
+
+#4. 평가, 예측
+    results = sess.run(hypothesis, feed_dict={x:x_predict})
+    print(sess.run(tf.math.argmax(results, 1)))
