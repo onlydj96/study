d8530c27d5e4ef9d7714349aa028745326214b1f
Bodong <94875061+onlydj96@users.noreply.github.com>
2022-02-11 17:41:54 +0900

    Add files via upload

--- tf114/tf18_mpl2_diabetes.py
+++ tf114/tf18_mpl2_diabetes.py
@@ -1,4 +1,4 @@
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import numpy as np
@@ -6,7 +6,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 tf.compat.v1.set_random_seed(66)
 
-datasets = load_boston()
+datasets = load_diabetes()
 x_data = datasets.data
 y_data = datasets.target.reshape(-1, 1)
 
@@ -18,7 +18,7 @@ x = tf.compat.v1.placeholder(tf.float32, shape=[None, x_data.shape[1]])
 y = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
 
 w = tf.compat.v1.Variable(tf.random.normal([x_data.shape[1], 64]), name='weight')
-b = tf.compat.v1.Variable(tf.random.normal([64]), name='bias') 
+b = tf.compat.v1.Variable(tf.random.normal([64]), name='bias')  
 
 #2. 모델구성
 hidden_layer1 = tf.sigmoid(tf.matmul(x, w) + b)
@@ -45,25 +45,27 @@ output = tf.matmul(hidden_layer4, w4) + b4
 
 #3-1. 컴파일
 loss = tf.reduce_mean(tf.square(output - y))    # mse
-optimizer = tf.compat.v1.train.AdamOptimizer()
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.001)
 train = optimizer.minimize(loss)
 
 #3-2. 훈련
 sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
-for epoch in range(10001):
+for epoch in range(5001):
     loss_val, w_val, b_val, _= sess.run([loss, w, b, train], feed_dict={x:x_train, y:y_train})
+    
     if epoch % 200 == 0:
         print(epoch, '\t', loss_val)
 
 
 #4. 평가, 예측
-y_predict = sess.run(output, feed_dict={x:x_test})
+y_predict = sess.run(output, feed_dict={x:x_test, y:y_test})
 
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_predict)
 print('r2스코어 : ', r2)
 
 sess.close()
-# r2스코어 :  0.6358970701808035
+
+# r2스코어 :  0.31792794273470937
