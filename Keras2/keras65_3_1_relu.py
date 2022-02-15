2e8e5668d3b1436c06ed267566f19371195e1cff
Bodong <94875061+onlydj96@users.noreply.github.com>
2022-02-15 17:45:01 +0900

    Add files via upload

---
+++ Keras2/keras65_3_1_relu.py
@@ -0,0 +1,15 @@
+
+# relu (Rectified Linear Unit)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def relu(x):
+    return np.maximum(0, x)
+    
+x = np.arange(-5, 5, 0.1)
+y = relu(x)
+
+plt.plot(x, y)
+plt.grid()
+plt.show()
