4ffbe3551787680b727daa88a1c06a45b48eee20
Bodong <94875061+onlydj96@users.noreply.github.com>
2022-02-15 17:45:17 +0900

    Add files via upload

---
+++ Keras2/keras65_4_softmax.py
@@ -0,0 +1,18 @@
+
+# softmax 
+# softmax의 합은 1
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x))
+
+x = np.arange(1, 5)
+y = softmax(x)
+
+ratio = y
+labels = y
+
+plt.pie(ratio, labels, shadow=True, startangle=90)
+plt.show()
